7c3b246954148f2af91555e55f561526fe9654a8
jarvisc1 <u7st69@gmail.com>
2019-09-20 11:26:33 +0430

    Cleaned up the setup script to be easier to maintain

--- R/setup.R
+++ R/setup.R
@@ -3,63 +3,28 @@
 # do not source this file
 
 
-install.packages("assertthat")
-install.packages("crayon")
-install.packages("data.table")
-install.packages("devtools")
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("ggthemes")
-install.packages("htmltools")
-install.packages("knitr")
-install.packages("magrittr")
-install.packages("purrr")
-install.packages("questionr")
-install.packages("reshape")
-install.packages("reshape2")
-install.packages("rmarkdown")
-install.packages("stringi")
-install.packages("stringr")
-install.packages("survey")
-install.packages("testthat")
-install.packages("tibble")
-install.packages("tidyr")
-install.packages("utils")
+pkgs <- c("assertthat","crayon","data.table","devtools",
+          "dplyr","ggplot2","ggthemes","htmltools","knitr",
+          "magrittr","purrr","questionr","reshape","reshape2",
+          "rmarkdown","stringi","stringr","survey","testthat",
+          "tibble","tidyr","utils", "here")
 
+install.packages(pkgs)
+
+# Install githubs packages 
+i_git <- function(pkg){
+  remotes::install_github(
+    pkg,
+    build_opts = c(),
+    build_vignettes = TRUE
+  )
+}
 
 Sys.setenv("TAR" = "internal")
 
-devtools::install_github(
-  "mabafaba/koboquest",
-  build_opts = c())
-
-devtools::install_github(
-  "mabafaba/kobostandards",
-  build_opts = c()
-)
-
-
-devtools::install_github(
-  "mabafaba/xlsformfill",
-  build_opts = c()
-)
-
-
-
-
-devtools::install_github(
-  "mabafaba/composr",
-  build_opts = c()
-)
-
-
-devtools::install_github(
-  "mabafaba/hypegrammaR",
-  build_opts = c(), build_vignettes = T, force = T
-)
-
-
-devtools::install_github(
-  "sharonorengo/visualisationIMPACT",
-  build_opts = c()
-)
+i_git("mabafaba/koboquest")
+i_git("mabafaba/kobostandards")
+i_git("mabafaba/xlsformfill")
+i_git("mabafaba/composr")
+i_git("mabafaba/hypegrammaR")
+i_git("sharonorengo/visualisationIMPACT")
