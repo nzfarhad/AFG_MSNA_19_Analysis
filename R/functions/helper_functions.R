b74f06aad4c3c944ed0657960efccd4d57dacd19
jarvisc1 <u7st69@gmail.com>
2019-09-20 11:13:04 +0430

    Added helper functions for analyses

---
+++ R/functions/helper_functions.R
@@ -0,0 +1,13 @@
+# helper functions
+
+
+
+# Takes in data and a list of vars in the format
+# c("var1", "var2", "var3", "var4") and spits out 
+# the sum of the row ignoring missing values
+compscore <- function(data, vars) {
+  rowsum(data[vars], na.rm = TRUE)
+  
+}
+
+
